8071d04c417e4f64b0bee4c2ecb310d4504d6cb5
beniregev <beniregev@gmail.com>
2025-07-12 01:51:59 +0300

    Build REST API using FastAPI - Add POST endpoint to all items and GET endpoint to get all items.

--- build-rest-api-using-fast-api/main.py
+++ build-rest-api-using-fast-api/main.py
@@ -2,6 +2,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+items = []
+
 @app.get("/")
 def root():
    return { "Hello": "World" }
+
+@app.get("/items")
+def get_all():
+   return items
+
+@app.post('/items')
+def create_item(item : str):
+   items.append(item)
+   return item
